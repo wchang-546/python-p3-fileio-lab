df35e31412329faf29272a4a6a7799ea674371d0
Will Chang <william.chaang@gmail.com>
2023-08-10 00:30:23 -0600

    Python read write practiced.

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w') as file_name: 
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a') as file_name: 
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as file_name: 
+        return file_name.read()
